dbe17aa138df15d5f45265173c92988c9ec45794
Войналович Владислав <woynaw@hotmail.com>
2023-04-28 11:23:32 +0300

    add Zadacha15

--- Zadacha15/Program.cs
+++ Zadacha15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Weekend(int arg)
+{
+    if (arg > 5) return true;
+    else return false;
+}
+bool Check(int arg)
+{
+    if (arg > 1 && arg <= 7) return true;
+    else
+    {
+        Console.WriteLine("Ошибка ввода");
+        return false;
+    }
+}
+
+Console.WriteLine("Введите день недели");
+int day = Convert.ToInt32(Console.ReadLine());
+if (Check(day))
+{
+    if (Weekend(day)) Console.WriteLine($"{day} -> Да");
+    else Console.WriteLine($"{day} -> Нет");
+}

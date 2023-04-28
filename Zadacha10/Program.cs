de9695056acf7914c2e4c61260bbd64afdee2d02
Войналович Владислав <woynaw@hotmail.com>
2023-04-28 10:55:24 +0300

    add Zadacha10

--- Zadacha10/Program.cs
+++ Zadacha10/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int digit(int arg) 
+{
+    return arg / 10 % 10;
+}
+
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{n} -> {digit(n)}");

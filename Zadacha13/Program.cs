da8081a383a323fda484bec5c906ddaf14a9e631
Войналович Владислав <woynaw@hotmail.com>
2023-04-28 11:08:56 +0300

    add Zadacha13

--- Zadacha13/Program.cs
+++ Zadacha13/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int digit(int arg)
+{
+    while (arg > 999)
+    {
+        arg /= 10;
+    }
+    return arg % 10;
+}
+
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n >= 100) Console.WriteLine($"{n} -> {digit(n)}");
+else Console.WriteLine("Третьей цифры нет");
